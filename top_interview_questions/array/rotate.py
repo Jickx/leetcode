b4e70f8c4f4bedc39ad698565e468af975120a01
Jickx <63093696+Jickx@users.noreply.github.com>
2022-07-12 10:36:25 +0300

    rotate inplace with insert

--- top_interview_questions/array/rotate.py
+++ top_interview_questions/array/rotate.py
@@ -1,18 +1,12 @@
 # Given an array, rotate the array to the right by k steps,
 # where k is non-negative.
 
-from collections import deque
-
-
 def rotate(nums, k):
-    aux = deque(nums)
-    aux.rotate(k)
-    nums.clear()
-    for i in aux:
-        nums.append(i)
-    print(nums)
+    for i in range(k):
+        nums.insert(0, nums.pop())
+    return nums
 
 
-rotate([1, 2, 3, 4, 5, 6, 7], 3)  # [5, 6, 7, 1, 2, 3, 4]
-rotate([-1, -100, 3, 99], 2)  # [3,99,-1,-100]
-rotate([1, 2], 3)  # [2, 1]
+assert rotate([1, 2, 3, 4, 5, 6, 7], 3) == [5, 6, 7, 1, 2, 3, 4]
+assert rotate([-1, -100, 3, 99], 2) == [3, 99, -1, -100]
+assert rotate([1, 2], 3) == [2, 1]
