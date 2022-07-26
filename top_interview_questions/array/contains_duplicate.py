2a04cf59ad6e6f2155701ace18edf70ee88db812
Jickx <63093696+Jickx@users.noreply.github.com>
2022-07-26 20:41:46 +0300

    add contains_duplicate

--- top_interview_questions/array/contains_duplicate.py
+++ top_interview_questions/array/contains_duplicate.py
@@ -2,8 +2,6 @@ def contains_duplicates(nums: list[int]) -> bool:
     return False if len(set(nums)) == len(nums) else True
 
 
-assert contains_duplicates([1, 2, 3, 4, 1]) == True
-assert contains_duplicates([1, 2, 3, 4]) == False
-assert contains_duplicates([1, 1, 1, 2, 3, 4]) == True
-
-
+assert contains_duplicates([1, 2, 3, 4, 1]) == True # noqa
+assert contains_duplicates([1, 2, 3, 4]) == False # noqa
+assert contains_duplicates([1, 1, 1, 2, 3, 4]) == True # noqa
