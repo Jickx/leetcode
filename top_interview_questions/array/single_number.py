2aa40950a8b0d212e04e2b91223d3ebea023d832
Jickx <63093696+Jickx@users.noreply.github.com>
2022-07-27 20:17:40 +0300

    linear complexity

--- top_interview_questions/array/single_number.py
+++ top_interview_questions/array/single_number.py
@@ -1,10 +1,15 @@
+# Given a non-empty array of integers nums, every element appears twice except
+# for one. Find that single one. You must implement a solution with a linear
+# runtime complexity and use only constant extra space.
+
+from collections import Counter
+
+
 def single_number(nums: list[int]) -> int:
-    while nums:
-        el = nums.pop()
-        if el in nums:
-            continue
-        else:
-            return el
+    ctr = Counter(nums)
+    for k, v in dict(ctr).items():
+        if v == 1:
+            return k
 
 
 assert single_number([2, 2, 1]) == 1
