23f168243934cdbe9d88c7497a24f6cf079eee24
Jickx <63093696+Jickx@users.noreply.github.com>
2022-07-27 21:34:50 +0300

    O(n2) option

--- top_interview_questions/array/intersect.py
+++ top_interview_questions/array/intersect.py
@@ -1,10 +1,18 @@
+# Given two integer arrays nums1 and nums2, return an array of their
+# intersection. Each element in the result must appear as many times
+# as it shows in both arrays and you may return the result in any order.
+
 def intersect(nums1: list[int], nums2: list[int]) -> list[int]:
     result = []
-    for i in nums1:
-        if i in nums2:
-            result.append(i)
+    while nums1:
+        el1 = nums1.pop()
+        for i, el2 in enumerate(nums2):
+            if el1 == el2:
+                nums2[i] = None
+                result.append(el1)
+                break
     return result
 
 
-assert intersect([2, 2, 1, 1], [2, 2]) == [2, 2]
-assert intersect([1, 2, 3, 4], [2, 3, 5]) == [2, 3]
+assert intersect([1, 2, 2, 1], [2, 2]) == [2, 2]
+assert intersect([4, 9, 5], [9, 4, 9, 8, 4]) == [4, 9] or [9, 4]
