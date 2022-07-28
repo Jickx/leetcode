5fc36a8b4eba88cc61ee804926f768af8011c29f
Jickx <63093696+Jickx@users.noreply.github.com>
2022-07-28 15:36:02 +0300

    add plus one

--- top_interview_questions/array/plus_one.py
+++ top_interview_questions/array/plus_one.py
@@ -1,12 +1,9 @@
 def plus_one(digits: list[int]) -> list[int]:
-    digits[-1] += 1
-    for i in range(len(digits) - 1, 0, -1):
-        if digits[i] == 10:
-            digits[i] = 0
-            digits[i - 1] += 1
-    return digits
+    number = int(''.join(str(i) for i in digits)) + 1
+    return [int(i) for i in str(number)]
 
 
 assert plus_one([1, 9]) == [2, 0]
 assert plus_one([1, 9, 9, 9, 9, 9, 9, 9]) == [2, 0, 0, 0, 0, 0, 0, 0]
 assert plus_one([4, 3, 2, 1]) == [4, 3, 2, 2]
+assert plus_one([9]) == [1, 0]
