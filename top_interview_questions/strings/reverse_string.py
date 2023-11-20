a2e889c946572f7121c3d34ef744faee270e639d
Jickx <valentin.sharonov@gmail.com>
2023-11-20 22:06:08 +0300

    add reverse_string

---
+++ top_interview_questions/strings/reverse_string.py
@@ -0,0 +1,21 @@
+def reverse_string(s: list[str]) -> None:
+    """
+    Do not return anything, modify s in-place instead.
+    """
+    left = 0
+    right = len(s) - 1
+    while left <= right:
+        temp = s[left]
+        s[left] = s[right]
+        s[right] = temp
+        left += 1
+        right -= 1
+    return
+
+
+s = ["h", "e", "l", "l", "o"]
+result = ["o", "l", "l", "e", "h"]
+
+reverse_string(s)
+
+assert s == result
