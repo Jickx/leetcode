e42e18ab6188138055e6f095f4e78bf0c23681ff
Jickx <valentin.sharonov@gmail.com>
2023-12-12 19:19:16 +0300

    linter

--- top_interview_questions/array/rotate_image.py
+++ top_interview_questions/array/rotate_image.py
@@ -1,6 +1,8 @@
-# You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+# You are given an n x n 2D matrix representing an image, rotate the image
+# by 90 degrees (clockwise).
 #
-# You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+# You have to rotate the image in-place, which means you have to modify the
+# input 2D matrix directly.
 # DO NOT allocate another 2D matrix and do the rotation.
 # n == matrix.length == matrix[i].length
 # 1 <= n <= 20
@@ -29,10 +31,7 @@ def rotate_image(matrix):
 
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-
 rotate_image(matrix)
 
 for line in matrix:
     ic(line)
-
-
