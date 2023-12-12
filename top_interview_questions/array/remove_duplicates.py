dee07e62c012b7c02d2620b974684d227dc4580a
Jickx <valentin.sharonov@gmail.com>
2023-12-12 19:03:34 +0300

    linter

--- top_interview_questions/array/remove_duplicates.py
+++ top_interview_questions/array/remove_duplicates.py
@@ -4,7 +4,7 @@ def remove_duplicates(nums):
     i = 0
     while i < len(nums) - 1:
         if nums[i] == nums[i + 1]:
-            del(nums[i])
+            del nums[i]
         else:
             i += 1
     return len(nums)
