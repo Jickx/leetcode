e320e72de58beaf6a706728522f42c67e1f943ef
Jickx <valentin.sharonov@gmail.com>
2023-12-14 17:23:22 +0300

    add linked list

--- top_interview_questions/linked_list/linked_list.py
+++ top_interview_questions/linked_list/linked_list.py
@@ -1,23 +1,23 @@
-class Node(object):
+class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
 
 
-class LinkedList(object):
+class LinkedList:
     def __init__(self, head):
         self.head = head
 
     def append(self, new_element):
         current = self.head
-        if self.head:
-            while current.next:
+        while True:
+            if current.next:
                 current = current.next
-            current.next = new_element
-        else:
-            current.next = new_element
+            else:
+                current.next = new_element
+                break
 
-    def print_linked_list(self):
+    def print_ll(self):
         current = self.head
         result = []
         while True:
@@ -26,9 +26,9 @@ class LinkedList(object):
                 current = current.next
             else:
                 break
-        return result
+        print(result)
 
-    def delete(self, value):
+    def delete_by_value(self, value):
         current = self.head
         while current:
             if current.value == value:
@@ -42,6 +42,24 @@ class LinkedList(object):
                 previous = current
                 current = current.next
 
+    def __len__(self):
+        current = self.head
+        count = 0
+        while current is not None:
+            current = current.next
+            count += 1
+        return count
+
+    def delete_by_index(self, index):
+        current = self.head
+        for _ in range(self.__len__() - index):
+            previous = current
+            current = current.next
+        if current == self.head:
+            self.head = current.next
+        else:
+            previous.next = current.next
+
 
 node1 = Node(1)
 node2 = Node(2)
@@ -50,12 +68,12 @@ node4 = Node(4)
 node5 = Node(5)
 
 ll = LinkedList(node1)
-
 ll.append(node2)
 ll.append(node3)
 ll.append(node4)
 ll.append(node5)
 
-ll.delete(2)
 
-print(ll.print_linked_list())
+ll.print_ll()
+ll.delete_by_index(1)
+ll.print_ll()
