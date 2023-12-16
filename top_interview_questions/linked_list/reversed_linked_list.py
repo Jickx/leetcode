1ce2b1f1c7c4b09d7ae22fe9ad55ba901df33cbb
Jickx <valentin.sharonov@gmail.com>
2023-12-16 20:59:05 +0300

    add reversed_linked_list.py

--- top_interview_questions/linked_list/reversed_linked_list.py
+++ top_interview_questions/linked_list/reversed_linked_list.py
@@ -4,19 +4,46 @@ class ListNode:
         self.next = next
 
 
-ll = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+# ll = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, ListNode(6))))))
+ll = ListNode(1, ListNode(2, ListNode(3)))
 
 
-def reverse_list(head: [ListNode]) -> [ListNode]:
-    current = head
-    prev = None
-    while current:
-        next_node = current.next
-        current.next = prev
-        prev = current
-        current = next_node
-    return prev
+def reverse_list(head):
+    def reverse(head, prev=None):
+        if not head:
+            return prev
+        n = head.next
+        head.next = prev
+        return reverse(n, head)
+
+    return reverse(head, prev=None)
+
+
+# def reverse_list(head):
+#     if not head:
+#         return None
+#
+#     new_head = head
+#     if head.next:
+#         new_head = reverse_list(head.next)
+#         head.next.next = head
+#     head.next = None
+#
+#     return new_head
+
+# def reverse_list(head):
+#     def reverse(cur, prev):
+#         if cur is None:
+#             return prev
+#         else:
+#             next = cur.next
+#             cur.next = prev
+#             return reverse(next, cur)
+#
+#     return reverse(head, None)
 
 
 x = reverse_list(ll)
-print(x)
+while x:
+    print(x.val)
+    x = x.next
